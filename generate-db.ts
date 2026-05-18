037438f04024ff149e2e8f52d5499a30642bc473
FireDasher <150294350+FireDasher@users.noreply.github.com>
2026-05-18 15:45:08 -0700

    Improve database generator, so you don't get rate limited, also it writes to the file automatically now

--- generate-db.ts
+++ generate-db.ts
@@ -1,13 +1,13 @@
 "use strict";
 import readline from "readline";
-import clipboardy from "clipboardy";
+import { readFile, writeFile } from "fs/promises";
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 const input: string[] = [];
 
 rl.on("line", line=>{
-	input.push(line);
+	input.push(line)
 });
 
 const bracket2diff = {
@@ -16,38 +16,57 @@ const bracket2diff = {
 	"(medium)": "Medium",
 	"(hard)": "Hard",
 	"(extreme)": "Extreme",
+};
+
+function keyDelimiterValueDelimiterKeyDelimiterValueFormatToObject(parts: string[]): Record<string, string> {
+	const obj: Record<string, string> = {};
+	for (let i = 0; i < parts.length; i += 2) obj[parts[i]] = parts[i+1];
+	return obj;
 }
 
-rl.on("close", ()=>{
-	const result = require("./gd-database.json");
+rl.on("close", async()=>{
+	const result: Record<string, Record<string, string>> = JSON.parse(await readFile("gd-database.json", "utf-8"));
+	// const result: Record<string, Record<string, string>> = {};
 	let diff = "unknown";
-	let finished = 0;
-	let total = 0;
-	for (const idx in input) {
-		const id = input[idx];
-		if (id === "") continue;
+
+	let diffs: Record<string, string> = {};
+	let ids: string[] = [];
+
+	for (let i = 0; i < input.length; ++i) {
+		const id = input[i];
+		if (id.trim().length === 0) continue;
 		if (id in bracket2diff) {
 			diff = bracket2diff[id as keyof typeof bracket2diff];
 		} else {
-			console.log("Fetching " + id);
-			total++;
-			fetch("https://gdbrowser.com/api/level/" + id).then(res=>{
-				res.json().then(e=>{
-					result[diff][id] = e.name;
-					finished++;
-				});
-			});
-			// result[diff][id] = "something";
-			// finished++;
+			ids.push(id);
+			diffs[id] = diff;
 		}
 	}
-	const checkdone = ()=>{
-		if (finished == total) {
-			clipboardy.writeSync(JSON.stringify(result, null, "\t"));
-			console.log("Wrote to clipbaord");
-		} else {
-			setTimeout(checkdone, 100);
-		}
-	};
-	checkdone();
+	
+	const res = await fetch("http://www.boomlings.com/database/getGJLevels21.php", {
+		method: "POST",
+		headers: {
+			"User-Agent": "",
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		body: new URLSearchParams({
+			"secret": "Wmfd2893gb7",
+			"type": "10",
+			"str": ids.join(",")
+		}).toString()
+	});
+	const value = await res.text();
+	console.log(value);
+
+	const levels = value.split("#", 1)[0].split("|");
+	const names = levels.map(levelObject => keyDelimiterValueDelimiterKeyDelimiterValueFormatToObject(levelObject.split(":")));
+	for (let i = 0; i < names.length; ++i) {
+		const id = names[i]["1"];
+		const name = names[i]["2"];
+		const diff = diffs[id];
+		if (typeof result[diff] == "undefined") result[diff] = {};
+		result[diff][id] = name;
+	}
+
+	await writeFile("gd-database.json", JSON.stringify(result, null, "\t"));
 });
